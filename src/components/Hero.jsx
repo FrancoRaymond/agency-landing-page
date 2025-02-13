528677ccb2b803fc2496375e35139981b13b6e97
FrancoRaymond <155026752+FrancoRaymond@users.noreply.github.com>
2025-02-14 01:33:31 +0200

    added navbar,hero section and assets

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import arrow from '../assets/images/icon-arrow-down.svg'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <h1>WE ARE CREATIVES</h1>
+        <img src={arrow} alt="" />
+      
+    </div>
+  )
+}
+
+export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import logo from "../assets/images/logo.svg";
+import menu from "../assets/images/icon-hamburger.svg";
 
 const Navbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
 
-export default Navbar
+  const handleMenuClick = () => {
+    setMenuOpen((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 500) {
+        setMenuOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div className="navbar">
+      <img src={logo} alt="" />
+      <nav className={menuOpen ? "active" : ""}>
+        <ul>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Projects</a></li>
+        </ul>
+        <button className="navBtn">Contact</button>
+      </nav>
+      <img
+        src={menu}
+        onClick={handleMenuClick}
+        alt=""
+        className="menu"
+      />
+    </div>
+  );
+};
+
+export default Navbar;
+
